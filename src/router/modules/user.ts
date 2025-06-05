7c5fe4d210c5d2c0991094f8d41765c0f7ec1b08
吴扬 <2396601748@qq.com>
2025-06-05 11:52:00 +0000

    update src/router/modules/user.ts.
    Signed-off-by: 吴扬 <2396601748@qq.com>

--- src/router/modules/user.ts
+++ src/router/modules/user.ts
@@ -1,12 +1,25 @@
-export default [
+import { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
     // {
     //     path: '/user',
     //     name: 'user',
     //     component: () => import('@/views/user.vue')
     // },
+     {
+        path: '/user',
+        redirect: '/user/my-info'
+    },
     {
         path: '/user/login',
         name: 'login',
         component: () => import('@/views/user/Login.vue')
+         },
+    {
+        path: '/user/my-info',
+        name: 'myInfo',
+        component: () => import('@/views/user/MyInfo.vue')
     }
 ]
+
+export default routes
