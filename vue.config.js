b5bc6202822286b85c2b212a05e23dba83c99574
nebular <3113631254@qq.com>
2026-02-22 23:58:32 +0800

    chore: 将开发服务器代理目标切换为远程API服务器
    将代理目标从本地开发服务器更改为远程服务器，以便在开发环境中测试与远程API的交互。

--- vue.config.js
+++ vue.config.js
@@ -13,8 +13,8 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        //target: 'http://111.228.38.111:5000',
-        target: 'http://127.0.0.1:5000',
+        target: 'http://111.228.38.111:5000',
+        //target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         pathRewrite: {
           '^': '' // 去掉前缀 `/api`
