5c86211b6af559ac8c4470fae10035155ea655e0
陈雨润 <chenyurun01@bilibili.com>
2026-06-07 10:14:13 +0800

    更新部署脚本中的部署目录；调整获取及显示及格线的API调用，增加size参数以限制返回数据量

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@
     [string]$Action = 'deploy',
 
     [string]$BuildDir = 'dist',
-    [string]$DeployDir = '/opt/huixue_frontend', # huixue_frontend_sangao;huixue_frontend
+    [string]$DeployDir = '/opt/huixue_frontend_sangao', # huixue_frontend_sangao;huixue_frontend
     [string]$DeployUser = 'root',
     [string]$DeployHost = '101.200.240.100',
     [switch]$UseUserSshConfig
